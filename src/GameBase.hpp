17b5f3c2e238ca9136f6092b1a25a72acf8da9c1
jenifermedeiross <jeniferraysa12.jr@gmail.com>
2023-06-17 19:22:30 -0300

    Adicionando funções que retornam limite da tela

--- src/GameBase.hpp
+++ src/GameBase.hpp
@@ -10,7 +10,9 @@
 class SpriteBase;
 
 class GameBase {
-
+    private:
+        int altura_tela = 54;
+        int largura_tela = 180;
     public:
         GameBase(){}
         virtual ~GameBase() {}
@@ -18,6 +20,9 @@ class GameBase {
         virtual void init() = 0;
         virtual void update() = 0;
         virtual void draw(SpriteBase &screen, unsigned x, unsigned y) = 0;
+
+        int ALTURA_TELA(){ return altura_tela; }
+        int LARGURA_TELA(){ return largura_tela; }
 };
 
 #endif

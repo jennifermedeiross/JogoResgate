2677af1437fee408d7a80aac505861d680c5a70b
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-06-18 19:04:10 -0300

    Criação da classe Pessoa derivada de ObjetoDeJogo

---
+++ src/Pessoa.hpp
@@ -0,0 +1,30 @@
+#ifndef PESSOA_HPP
+#define PESSOA_HPP
+
+#include "ASCII_ENGINE/ObjetoDeJogo.hpp"
+
+class Pessoa : public ObjetoDeJogo
+{
+    private:
+        double peso;
+        bool resgatada;
+
+    public:
+        Pessoa(Sprite sprite, double peso, unsigned x, unsigned y) :
+            ObjetoDeJogo(sprite, x, y), peso(peso), resgatada(false) {}
+
+        Pessoa(SpriteAnimado sprite, double peso, unsigned x, unsigned y) :
+            ObjetoDeJogo(sprite, x, y), peso(peso), resgatada(false) {}
+        
+        virtual ~Pessoa(){}
+        
+        double getPeso() { return this->peso; }
+        void setPeso(double peso) { this->peso = peso; }
+
+        void resgatar(){
+            this->resgatada = true;
+            this->desativar();
+        }
+};
+
+#endif

56aedddce876770dffb4511d372cacaac8c7ae7f
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-06-25 16:45:54 -0300

    Instanciando classe Game

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,7 @@
-#include "ASCII_ENGINE/SpriteBase.hpp"
-#include "ASCII_ENGINE/Sprite.hpp"
-#include "ASCII_ENGINE/SpriteAnimado.hpp"
-#include "ASCII_ENGINE/ObjetoDeJogo.hpp"
-#include "ASCII_ENGINE/Fase.hpp"
-#include "Pessoa.hpp"
-#include "Heroi.hpp"
-#include "Combustivel.hpp"
-#include "Base.hpp"
-#include "FaseGame.hpp"
-#include "FaseMenu.hpp"
+#include "Game.hpp"
 
 int main() {
-    FaseGame fase("FaseGame");
-    fase.run();
+    Game().run();
 
     return 0;
 }

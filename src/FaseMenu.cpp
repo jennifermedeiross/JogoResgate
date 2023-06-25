264d8c00b1d0ec71fcdec9c94d0244d89eeb68dc
jennifermedeiross <jenniferraysa12.jr@gmail.com>
2023-06-25 16:43:24 -0300

    Implementação da FaseMenu

--- src/FaseMenu.cpp
+++ src/FaseMenu.cpp
@@ -1,49 +1,116 @@
-// #include "FaseMenu.hpp"
+#include "FaseMenu.hpp"
 
-// void FaseMenu::run(){
-//     Sprite screen("src/imgs/screen.txt");
-//     Sound somMenu("src/musics/menu.mp3");
-//     Sound selecionou("src/musics/salvou.mp3");
+void FaseMenu::run(){
+    Sprite screen("src/imgs/screen.txt");
+    Sprite mHelp("src/imgs/mensagemHelp.txt");
+    Sprite mAbout("src/imgs/mensagemAbout.txt");
+    Sound somMenu("src/musics/menu.mp3");
+    Sound selecionou("src/musics/salvou.mp3");
 
-//     init();
-//     somMenu.playloop();
+    init();
+    somMenu.playloop();
 
-//     while (true) {
+    while (true) {
         
-//         system("clear");
-
-//         draw(screen, 0 ,0);
-//         show(&screen);
-//         char entrada = getTeclado().getch();
-
-//         if(entrada == 'w' || entrada == 'W'){
-//             if(pArrow->getLinha() < pStart->getLinha())
-//                 pArrow->moveTo(21);
-//         }
-//     }
-// }
-
-// void FaseMenu::init(){
-//     Sprite screen("src/imgs/screen.txt");
-//     Sprite helicoptero("src/imgs/helicopteroMenu.txt");
-
-//     helicoptero.draw(screen, 5, 66);
-
-//     this->setBackground(&screen);
-
-//     std::list<ObjetoDeJogo*> lista;
-
-//     pStart = new Text(ObjetoDeJogo(Sprite("src/imgs/start.txt"), 20, 78));
-//     pHelp = new Text(ObjetoDeJogo(Sprite("src/imgs/start.txt"), 25, 78));
-//     pExit = new Text(ObjetoDeJogo(Sprite("src/imgs/exit.txt"), 30, 78));
-//     pArrow = new Text(ObjetoDeJogo(Sprite("src/imgs/seta.txt"), 18, 78));
-
-//     lista.push_back(pStart);
-//     lista.push_back(pHelp);
-//     lista.push_back(pExit);
-//     lista.push_back(pArrow);
+        system("clear");
+        draw(screen, 0 ,0);
+        show(&screen);
+        char entrada = getTeclado().getch();
 
 
-//     this->setListaObjetos(lista);
+        if( entrada == 'q' || entrada == 'Q'){
+            setState("end");
+            break;
+        }
 
-// }
+        else if( entrada == 'd' || entrada == 'D'){
+            if((pSelector->getColuna()+pSelector->getSpriteObj()->getLargura())  < (pExit->getColuna() - 2)){
+                moveRigth(*pSelector);
+            }
+        }
+
+        else if( entrada == 'a' || entrada == 'A'){
+            if(pSelector->getColuna() > pStart->getColuna()){
+                moveLeft(*pSelector);
+            }
+        }
+
+        else if (entrada == 'x' || entrada == 'X'){
+
+            if(pSelector->colideCom(*pStart)){
+                selecionou.play();
+                sleep(1);
+                setState("FaseGame");
+                break;
+            }
+
+            else if(pSelector->colideCom(*pHelp)){
+                selecionou.play();
+                system("clear");
+                mHelp.draw(screen, 43, 12);
+                show(&screen);
+                std::cin.ignore();
+            }
+
+            else if(pSelector->colideCom(*pAbout)){
+                selecionou.play();
+
+                mAbout.draw(screen, 43, 16);
+
+                system("clear");
+                show(&screen);
+                std::cin.ignore();
+            }
+
+            else if(pSelector->colideCom(*pExit)){
+                setState("end");
+                break;
+            }
+        }
+
+
+    }
+}
+
+
+void FaseMenu::init(){
+    Sprite screen("src/imgs/screen.txt");
+    Sprite gameName("src/imgs/gameName.txt");
+    Sprite start("src/imgs/Menustart.txt");
+    Sprite help("src/imgs/Menuhelp.txt");
+    Sprite about("src/imgs/Menuabout.txt");
+    Sprite exit("src/imgs/Menuexit.txt");
+    Sprite selector("src/imgs/selector.txt");
+
+    gameName.draw(screen, 58, 15);
+
+    this->setBackground(&screen);
+
+    std::list<ObjetoDeJogo *> lista;
+
+    pStart = new ObjetoDeJogo(start, 23, 32);
+    pHelp = new ObjetoDeJogo(help, 63, 32);
+    pAbout = new ObjetoDeJogo(about, 103, 32);
+    pExit = new ObjetoDeJogo(exit, 143, 32);
+    pSelector = new ObjetoDeJogo(selector, 14, 33);
+
+    lista.push_back(pStart);
+    lista.push_back(pHelp);
+    lista.push_back(pAbout);
+    lista.push_back(pExit);
+    lista.push_back(pSelector);
+
+    setListaObjetos(lista);
+}
+
+void FaseMenu::moveRigth(ObjetoDeJogo &obj){
+    for(int i=0; i < 20; i++){
+        obj.moveRigth();
+    }
+}
+
+void FaseMenu::moveLeft(ObjetoDeJogo &obj){
+    for(int i=0; i < 20; i++){
+        obj.moveLeft();
+    }
+}
--- src/FaseMenu.hpp
+++ src/FaseMenu.hpp
@@ -1,28 +1,36 @@
-// #ifndef FASEMENU_HPP
-// #define FASEMENU_HPP
+#ifndef FASEMENU_HPP
+#define FASEMENU_HPP
 
-// #include "Fase.hpp"
-// #include "Text.hpp"
+#include "ASCII_ENGINE/Fase.hpp"
 
-// class FaseMenu : public Fase{
-//     private:
-//         Text *pStart;
-//         Text *pHelp;
-//         Text *pExit;
-//         Text *pArrow;
+class FaseMenu : public Fase{
 
-//     public:
-//         FaseMenu(std::string state) : Fase(state){}
-//         ~FaseMenu(){}
+    private:
+        ObjetoDeJogo *pStart;
+        ObjetoDeJogo *pHelp;
+        ObjetoDeJogo *pAbout;
+        ObjetoDeJogo *pExit;
+        ObjetoDeJogo *pSelector;
 
-//         virtual void run();
+    public:
+        FaseMenu(std::string state) : Fase(state){}
+        ~FaseMenu(){
+            Fase::~Fase();
+            delete pStart;
+            delete pHelp;
+            delete pAbout;
+            delete pExit;
+            delete pSelector;
+        }
 
-//         //GAMEBASE
-//         virtual void init();
+        virtual void run();
+        void moveRigth(ObjetoDeJogo &);
+        void moveLeft(ObjetoDeJogo &);
+
+        //GAMEBASE
+        virtual void init();
 
 
-// };
+};
 
-
-
-// #endif
+#endif
